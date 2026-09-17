9a7e310d0be0dae7c6778e841dfa75d6f44f49f4
dipu695 <dipupaul695@gmail.com>
2026-09-17 16:10:02 +0600

    s

---
+++ Sorting/Sorting/merge_sort.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void mergeSort(vector<int> &arr, int st, int end)
+{
+    if (st < end)
+    {
+        int mid = st + (end - st) / 2;
+        mergeSort(arr, st, mid);      // left half
+        mergeSort(arr, mid + 1, end); // right half
+        merge(arr, st, mid, end);
+    }
+}
+void merge(vector<int> &arr, int st, int mid, int end)
+{
+    vector<int> temp;
+    int i = st, j = mid + 1;
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else
+        {
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    while(i <= mid){
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while(j <= end){
+        temp.push_back(arr[j]);
+        j++;
+    }
+    for(int idx = 0 ; idx < temp.size() ; idx++){
+        arr[idx+st] = temp[idx];
+    }
+}
+
+int main()
+{
+    vector<int>arr = {20 , 9 , 1 , 5 , 70 , 60};
+    mergeSort(arr  , 0 , arr.size()-1);
+    for(int val : arr){
+        cout << val <<" ";
+    }
+    cout << endl;
+    return 0;
+}
